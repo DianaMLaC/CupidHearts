29b5637ccacd95afb30a1144ad00eb9b88789cf0
Diana <mada.chirica@gmail.com>
2025-03-24 15:08:17 +0000

    Updates

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const moving_object_1 = __importDefault(require("./moving_object"));
+window.MovingObject = moving_object_1.default;
+//# sourceMappingURL=index.js.map
---
+++ moving_object.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class MovingObject {
+    constructor(config) {
+        this.pos = config.pos;
+        this.vel = config.vel;
+        this.radius = config.radius;
+        this.color = config.color;
+    }
+}
+exports.default = MovingObject;
+//# sourceMappingURL=moving_object.js.map
